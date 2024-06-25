2e835b0403b1f35b809f0c96e1cd27fa238fe419
fbansept <bansept.franck@gmail.com>
2024-06-25 14:58:47 +0200

    first

--- src/main/java/edu/fbansept/demo/controller/UtilisateurController.java
+++ src/main/java/edu/fbansept/demo/controller/UtilisateurController.java
@@ -3,38 +3,57 @@ package edu.fbansept.demo.controller;
 import edu.fbansept.demo.dao.UtilisateurDao;
 import edu.fbansept.demo.model.Utilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/utilisateur")
 public class UtilisateurController {
 
     @Autowired
     UtilisateurDao utilisateurDao;
 
-    @GetMapping("/utilisateur/liste")
+    @GetMapping("/liste")
     public List<Utilisateur> liste() {
         return utilisateurDao.findAll();
     }
 
-    @GetMapping("/utilisateur")
-    public Utilisateur get() {
+    @GetMapping("/{id}")
+    public ResponseEntity<Utilisateur> get(@PathVariable int id) {
 
-        Utilisateur franck = new Utilisateur();
-        franck.setId(4);
-        franck.setEmail("a@a.com");
-        franck.setPassword("root");
+        Optional<Utilisateur> optionalUtilisateur = utilisateurDao.findById(id);
 
-        return franck;
+        if(optionalUtilisateur.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalUtilisateur.get(),HttpStatus.OK);
     }
 
-    @PostMapping("/utilisateur")
-    public boolean add(@RequestBody Utilisateur utilisateur) {
+    @PostMapping("")
+    public ResponseEntity<Utilisateur> add(@RequestBody Utilisateur utilisateur) {
+        utilisateur.setId(null);
+        utilisateurDao.save(utilisateur);
+        return new ResponseEntity<>(utilisateur, HttpStatus.CREATED);
+    }
 
+    @PutMapping("/{id}")
+    public boolean update(
+            @PathVariable int id,
+            @RequestBody Utilisateur utilisateur) {
+
+        utilisateur.setId(id);
+        utilisateurDao.save(utilisateur);
+        return true;
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable int id) {
+        utilisateurDao.deleteById(id);
         return true;
     }
 
